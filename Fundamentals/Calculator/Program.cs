ec1f3fcdcc8e00b5c947fa80cbb64a181e3026f4
Meani Freitas <meani.sf@gmail.com>
2025-03-14 11:57:07 -0300

    Implement a calculator using switch case

---
+++ Fundamentals/Calculator/Program.cs
@@ -0,0 +1,34 @@
+double n1, n2, result;
+char op;
+Console.WriteLine("Input first value: ");
+n1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Input second value: ");
+n2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Select an operation: ");
+Console.WriteLine("+ ---> sum\n- ---> subtract");
+Console.WriteLine("* ---> multiply\n/ ---> divide");
+op = char.Parse(Console.ReadLine());
+
+switch (op)
+{
+
+    case '+':
+        result = n1 + n2;
+        Console.WriteLine(n1 + " + " + n2 + " = " + result);
+        break;
+    case '-':
+        result = n1 - n2;
+        Console.WriteLine(n1 + " - " + n2 + " = " + result);
+        break;
+    case '*':
+        result = n1 * n2;
+        Console.WriteLine(n1 + " * " + n2 + " = " + result);
+        break;
+    case '/':
+        result = n1 / n2;
+        Console.WriteLine(n1 + " / " + n2 + " = " + result);
+        break;
+    default:
+        Console.WriteLine("error");
+        break;
+}
